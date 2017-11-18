aed63c7f7a9e112c53fcc952646193b62eee7ca6
buibup <b5209194@gmail.com>
2017-11-18 10:46:51 +0700

    complete with Working With Data (Part II)

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,25 @@
+﻿using OdeToFood.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    //[Authorize]
+    [Log]
+    public class CuisineController : Controller
+    {
+        
+        public ActionResult Search(string name = "BUI")
+        {
+            throw new Exception("Something terrible has happended");
+
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+        }
+        
+    }
+}
